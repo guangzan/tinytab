25e55295a5d2ca9da0b4e8d59b86bdbd24075d7e
guangzan <923665892@qq.com>
2021-07-10 14:50:07 +0800

    auto deploy

---
+++ docs/assets/About.0fed89d6.js
@@ -0,0 +1,1 @@
+import{o as a,b as e,e as o}from"./vendor.fce71c70.js";const s={},n={class:"about"},r=o("h1",null,"This is an about page",-1);s.render=function(o,s){return a(),e("div",n,[r])};export default s;
