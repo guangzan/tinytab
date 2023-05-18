84851d4cf90490c9de2255dc83569d13152c9565
guangzan <guangzan1999@outlook.com>
2023-05-18 09:35:03 +0800

    auto deploy

--- docs/assets/About.41085366.js
+++
@@ -1,1 +0,0 @@
-import{o as a,b as s,m as o}from"./vendor.6192496b.js";const n={},r={class:"about"},t=[o("h1",null,"This is an about page",-1)];n.render=function(o,n){return a(),s("div",r,t)};export{n as default};
---
+++ docs/assets/About.4de56dd5.js
@@ -0,0 +1,1 @@
+import{o as a,b as s,m as o}from"./vendor.8ff6b461.js";const n={},r={class:"about"},t=[o("h1",null,"This is an about page",-1)];n.render=function(o,n){return a(),s("div",r,t)};export{n as default};
