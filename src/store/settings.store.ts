5501185c1758e0604176a3c02ca05da5b787afdb
guangzan <guangzan1999@outlook.com>
2023-05-29 01:08:07 +0800

    refactor: `UpdateEngine` in settings store

--- src/store/settings.store.ts
+++ src/store/settings.store.ts
@@ -6,6 +6,7 @@ import type * as TT from '@/types'
 import { removeArrItem } from '@/utils/tools'
 import { initialSettings } from '@/constants/settings'
 import { STORAGE_KEY_PREFIX } from '@/constants/keys'
+import { Noti } from '@/composables/notification'
 
 export const useSettingsStore = defineStore('settings', () => {
   const settings = useStorageAsync<TT.TTSettings>(`${STORAGE_KEY_PREFIX}_SETTINGS`, initialSettings)
@@ -39,37 +40,40 @@ export const useSettingsStore = defineStore('settings', () => {
         data.splice(index, 1)
     })
   }
-  function UpdateEngine(newItem: TT.TTEngine): Promise<TT.TTMsgItem[]> {
-    return new Promise((resolve) => {
-      const resultMsg: Array<TT.TTMsgItem> = []
-      if (newItem.isDefault === true) {
-        settings.value.engines.forEach((item: TT.TTEngine) => {
-          const updateengines = (item: TT.TTEngine, newItem: TT.TTEngine): void => {
-            Object.assign(item, newItem)
-            UpdateEngines(settings.value.engines)
-          }
-          if (newItem.isDefault === false) {
-            if (item.id === newItem.id) {
-              if (item.isDefault === true) {
-                resultMsg.push({
-                  type: 'error',
-                  content: '不能将默认搜索引擎取消',
-                })
-                newItem.isDefault = true
-              }
-            }
-          }
-          if (item.id === newItem.id) {
-            resultMsg.push({
-              type: 'success',
-              content: '修改成功',
-            })
-            resolve(resultMsg)
-            updateengines(item, newItem)
-          }
-        })
+  function UpdateEngine(newItem: TT.TTEngine) {
+    let isEditDefaultEngine = false
+    if (!newItem.isDefault) {
+      for (let i = 0; i < settings.value.engines.length; i++) {
+        const item = settings.value.engines[i]
+        if (item.id === newItem.id && item.isDefault === true) {
+          Noti({
+            type: 'warning',
+            content: '无法将默认搜索引擎取消, 已恢复此项设置-[设为默认]，其他设置项已保存',
+          })
+          isEditDefaultEngine = true
+          newItem.isDefault = true
+        }
       }
+    }
+    if (newItem.isDefault) {
+      settings.value.engines = settings.value.engines.map((engine) => {
+        if (engine.isDefault)
+          engine.isDefault = false
+
+        return engine
+      })
+    }
+    settings.value.engines = settings.value.engines.map((engine) => {
+      if (engine.id === newItem.id)
+        return newItem
+      return engine
     })
+    if (!isEditDefaultEngine) {
+      Noti({
+        type: 'success',
+        content: '修改成功',
+      })
+    }
   }
   function UpdateEngines(value: TT.TTEngine[]) {
     settings.value.engines = value
